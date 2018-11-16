7dd006331ead26cdd31bd0e00d56954ce7a36c97
photograph <1239417124.com>
2018-11-16 16:04:09 +0800

    add init-packages.el

--- init.el
+++ init.el
@@ -4,64 +4,14 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (package-initialize)
-  (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-			   ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
-;; 注意 elpa.emacs-china.org 是 Emacs China 中文社区在国内搭建的一个 ELPA 镜像
-
-(setq package-selected-packages 'photograph/packages)
+(package-initialize)
 
 
-;; cl - Common Lisp Extension
-(require 'cl)
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+(require 'init-packages)
 
-;; Add Packages
-;; 定义了一个package列表
-(defvar photograph/packages '(
-		;; --- Auto-completion ---
-		company
-		;; --- Better Editor ---
-		hungry-delete
-		swiper
-		counsel;; swiper的依赖包
-		smartparens
-		popwin
-		;; --- Major Mode ---
-		js2-mode
-		;; --- Minor Mode ---
-		nodejs-repl
-		exec-path-from-shell;; mac上要找到命令行的程序 要安装这个
-		;; --- Themes ---
-		monokai-theme
-		solarized-theme
-		) "Default packages")
-
-(setq package-selected-packages photograph/packages)
-
-;; 判断"photograph/packages"这个变量里所有的package是否已经全部安装
-;; 如果全部安装完了 则返回nil
-(defun photograph/packages-installed-p ()
-  ;; loop for 是Common Lisp Extension里面的
-     (loop for pkg in photograph/packages
-	   when (not (package-installed-p pkg)) do (return nil)
-	   finally (return t)))
-
- (unless (photograph/packages-installed-p)
-     (message "%s" "Refreshing package database...")
-     (package-refresh-contents)
-     (dolist (pkg photograph/packages)
-       (when (not (package-installed-p pkg))
-	 (package-install pkg))))
-
-
-;; Find Executable Path on OS X
-;; 当系统是mac时 则初始化下面的package
-;; node-js-repl
-;; node-js-repl-send-buffer
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
+;; 取消移动到文件头部或尾部时候的提示音
+(setq ring-bell-function 'ignore) 
 
 (tool-bar-mode -1)
 
@@ -77,7 +27,7 @@
 
 (recentf-mode t)
 
-(global-company-mode t)
+
 
 ;; 改变光标样式
 ;; setq-default
@@ -119,14 +69,10 @@
 ;; 加载主题
 (load-theme 'monokai t)
 
-(require 'hungry-delete)
-(global-hungry-delete-mode) 
 
-;; config for swiper
-;; C-n or C-p to choose
-;; C-x b to choose recent file
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
+
+
+
 (global-set-key "\C-s" 'swiper)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 (global-set-key (kbd "<f6>") 'ivy-resume)
@@ -135,10 +81,7 @@
 (global-set-key (kbd "C-h f") 'counsel-describe-function)
 (global-set-key (kbd "C-h v") 'counsel-describe-variable)
 
-;; config for smartparens
-(require 'smartparens-config)
-;; (add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
-(smartparens-global-mode t)
+
 
 ;; C-h f 是查找函数的描述
 ;; C-h C-h 是查找函数在哪里
@@ -146,11 +89,7 @@
 (global-set-key (kbd "C-h C-v") 'find-variable)
 (global-set-key (kbd "C-h C-k") 'find-function-on-key)
 
-;; config js2-mode for js files
-(setq auto-mode-alist
-      (append
-       '(("\\.js\\'" . js2-mode))
-       auto-mode-alist))
+
 
 
 ;; org-mode
@@ -161,13 +100,18 @@
 ;; 当在外部修改此文件时，自动加载
 (global-auto-revert-mode t)
 
-(require 'popwin)
-(popwin-mode 1)
+;; 设置缩写
+(abbrev-mode 1)
+(define-abbrev-table 'global-abbrev-table '(
+					   ;; signature
+					   ("chp" "photograph")
+					   ;; emacs regex
+					   
+					   ))
 
 
 ;; add(set-language-environment 'utf-8)
 ;; (set-locale-environment "UTF-8")
-
 
 
 (custom-set-variables
---
+++ lisp/init-packages.el
@@ -0,0 +1,99 @@
+(when (>= emacs-major-version 24)
+  ;; 下面两行在init.el文件开头已经有了
+  ;;(require 'package)
+  ;;(package-initialize)
+  (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+			   ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
+;; 注意 elpa.emacs-china.org 是 Emacs China 中文社区在国内搭建的一个 ELPA 镜像
+
+(setq package-selected-packages 'photograph/packages)
+
+
+;; cl - Common Lisp Extension
+(require 'cl)
+
+;; Add Packages
+;; 定义了一个package列表
+(defvar photograph/packages '(
+		;; --- Auto-completion ---
+		company
+		;; --- Better Editor ---
+		hungry-delete
+		swiper
+		counsel;; swiper的依赖包
+		smartparens
+		popwin
+		multiple-cursors
+		;; --- Major Mode ---
+		js2-mode
+		;; --- Minor Mode ---
+		nodejs-repl
+		exec-path-from-shell;; mac上要找到命令行的程序 要安装这个
+		;; --- Themes ---
+		monokai-theme
+		solarized-theme
+		) "Default packages")
+
+(setq package-selected-packages photograph/packages)
+
+;; 判断"photograph/packages"这个变量里所有的package是否已经全部安装
+;; 如果全部安装完了 则返回nil
+(defun photograph/packages-installed-p ()
+  ;; loop for 是Common Lisp Extension里面的
+     (loop for pkg in photograph/packages
+	   when (not (package-installed-p pkg)) do (return nil)
+	   finally (return t)))
+
+ (unless (photograph/packages-installed-p)
+     (message "%s" "Refreshing package database...")
+     (package-refresh-contents)
+     (dolist (pkg photograph/packages)
+       (when (not (package-installed-p pkg))
+	 (package-install pkg))))
+
+
+;; Find Executable Path on OS X
+;; 当系统是mac时 则初始化下面的package
+;; node-js-repl
+;; node-js-repl-send-buffer
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+
+
+;; config for smartparens
+(require 'smartparens-config)
+;; (add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
+(smartparens-global-mode t)
+
+
+;; config for swiper
+;; C-n or C-p to choose
+;; C-x b to choose recent file
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+
+
+;; config js2-mode for js files
+(setq auto-mode-alist
+      (append
+       '(("\\.js\\'" . js2-mode))
+       auto-mode-alist))
+
+
+(global-company-mode t)
+
+
+;; 加载主题
+(load-theme 'monokai t)
+
+
+;; when require, wh(setq company-minimum-prefix-length 1)en not require
+(require 'popwin)
+(popwin-mode 1)
+
+
+(provide 'init-packages)
